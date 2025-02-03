ffd6c45c4ec4c0cd1ee8e2c84ba84c0b710e2e30
Andrey9019 <1234andrey5678@gmail.com>
2025-02-03 15:53:01 +0200

    setting project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#1E293B",
+      secondary: "#FACC15",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
