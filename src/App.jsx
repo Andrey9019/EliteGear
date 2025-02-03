02922035bd63f0c15f011f007dfcf75b544d3ec3
Andrey9019 <1234andrey5678@gmail.com>
2025-02-03 15:59:02 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className=" flex flex-col items-center">
         <p className=""> {count}</p>
-        <h1 className=""> Hello World</h1>
+        <h1 className=""> Hello World!</h1>
         <button
           onClick={() => setCount((count) => count + 1)}
           className="font-sans bg-primary text-secondary hover:bg-secondary hover:text-primary p-3 w-fit rounded-full cursor-pointer"
