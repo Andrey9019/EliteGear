7fab2e4cc4a351c710a26f0ab6eaa90e802c6d3c
Andrey9019 <1234andrey5678@gmail.com>
2025-02-26 15:36:09 +0200

    feat: component btn

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import Container from "../components/layout/Container";
+
+const MainPage = () => (
+  <Container>
+    <h1 className="">Hello world</h1>
+  </Container>
+);
+
+export default MainPage;
